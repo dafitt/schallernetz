be8d69e133bf58a7e9081da45b072ead2a6a8804
DigitalRobot <digitalrobot@outlook.de>
2024-01-25 14:30:49 +0100

    feat: DavidCAL

--- hosts/minisforumhm80/default.nix
+++ hosts/minisforumhm80/default.nix
@@ -9,6 +9,7 @@
 
     ../../services/DavidSYNC.nix
     ../../services/DavidVPN.nix
+    ../../services/DavidCAL.nix
     ../../services/MichiSHARE.nix
     ../../services/pihole.nix
     ../../services/searx.nix
