0b13c919b908c79b2b05f581437330c8100a30d1
DigitalRobot <digitalrobot@outlook.de>
2024-02-06 18:14:33 +0100

    haproxy: ssl certificate with agenix

--- services/haproxy.nix
+++ services/haproxy.nix
@@ -14,26 +14,34 @@ let cfg = config.services.haproxy; in
 
   config = {
 
+    age.secrets."minisforumhm80_proxy.pem" = {
+      file = ../secrets/minisforumhm80_proxy.pem.age;
+      owner = "haproxy";
+      group = "haproxy";
+    };
+
     # Reverse Proxy before the application servers
     # [HAProxy config tutorials](https://www.haproxy.com/documentation/haproxy-configuration-tutorials/core-concepts/overview/)
-    # TODO [Essential Configuration](https://www.haproxy.com/blog/the-four-essential-sections-of-an-haproxy-configuration)
-    # TODO run in a container to reduce network attack surface
+    # [Essential Configuration](https://www.haproxy.com/blog/the-four-essential-sections-of-an-haproxy-configuration)
+    # TODO run in a container with own ip to reduce network attack surface
     services.haproxy = {
       enable = true;
       config = ''
         global
           daemon
+          maxconn 5000
+          ssl-default-bind-options ssl-min-ver TLSv1.2 no-tls-tickets
 
         defaults
           mode http
-          timeout connect 5s
-          timeout client 50s
-          timeout server 50s
+          timeout connect 10s
+          timeout client 30s
+          timeout server 30s
 
         frontend www
           bind [::]:80 v4v6
-          bind [::]:443 v4v6 #TODO ssl crt /site.pem
-          #http-request redirect scheme https unless { ssl_fc }
+          bind [::]:443 v4v6 ssl crt ${config.age.secrets."minisforumhm80_proxy.pem".path}
+          http-request redirect scheme https unless { ssl_fc }
 
           ${builtins.concatStringsSep "\n  " cfg.frontends.www.extraConfig}
       '';
