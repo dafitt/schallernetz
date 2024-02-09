aae16398b8f950032b46a425a336daeede2ed091
DigitalRobot <digitalrobot@outlook.de>
2024-02-09 08:00:13 +0100

    agenix: refactor haproxy

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,19 +4,23 @@
 let
   # public ssh keys of machines, that will have access to the secrets
   # the corresponding private keys will be able to decrypt them
-  # grab them through #$ ssh-keyscan user@host
-  keys = [
-    "***REMOVED_SSH-PUBLICKEY***" # david@DavidDESKTOP
-    "***REMOVED_SSH-PUBLICKEY***" # david@DavidLEGION
-    "***REMOVED_SSH-PUBLICKEY***" # minisforumhm80
-  ];
+  # grab them through #$ ssh-keyscan host
+
+  # USERS
+  david = "***REMOVED_SSH-PUBLICKEY***";
+
+  # MACHINES
+  minisforumhm80 = "***REMOVED_SSH-PUBLICKEY***"; # minisforumhm80
 in
 {
-  # 1. New entry with allowed keys `"secret1.age".publicKeys = keys;`
-  # 2. Create the secret file #$ nix run github:ryantm/agenix -- -e secret1.age
-  # 3. Import to your NixOS configuration `age.secrets."secret1".file = ../secrets/secret1.age;`
-  # 4. Use it with `config.age.secrets."secret1".path;`
+  # 1. New entry with allowed keys `"FILE.age".publicKeys = keys;`
+  # 2. Create the secret file #$ nix run github:ryantm/agenix -- -e FILE.age
+  # 3. Import to your NixOS configuration `age.secrets."FILE".file = ../secrets/FILE.age;`
+  # 4. Use it with `config.age.secrets."FILE".path;`
 
-  "minisforumhm80_proxy.pem.age".publicKeys = keys;
-  "searx.age".publicKeys = keys;
+  "haproxy-www-ssl.pem.age".publicKeys = [ david minisforumhm80 ];
+  "searx.age".publicKeys = [ david minisforumhm80 ];
+
+  # Edit #$ nix run github:ryantm/agenix -- -e FILE -i PRIVATE_KEY
+  # Rekey #$ nix run github:ryantm/agenix -- -r -i PRIVATE_KEY
 }
