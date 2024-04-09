3875036608d3c4ef42fed7e028bfdf1ce2fc7bb2
dafitt <dafitt@posteo.me>
2024-04-09 11:42:11 +0200

    docs: updated commands

--- templates/system/default.nix
+++ templates/system/default.nix
@@ -1,15 +1,12 @@
 { ... }: {
-  #$ nix build .#nixosConfigurations.<name>.config.system.build.toplevel
-  #$ nixos-rebuild build --flake .[#<name>] --show-trace
-  #$ nixos-rebuild build-vm --flake .[#<name>]
-
-  #$ sudo nixos-rebuild test --flake .[#<name>]
-  #$ sudo nixos-rebuild switch --flake .[#<name>]
-  #$ sudo nixos-rebuild boot --flake .[#<name>]
+  #$ nix repl .#nixosConfigurations.<host>
+  #$ nix build .#nixosConfigurations.<host>.config.system.build.toplevel
+  #$ nixos-rebuild build --flake .#<host> --show-trace
+  #$ sudo nixos-rebuild <test|switch|boot> --flake .#<host>
 
   imports = [ ./hardware-configuration.nix ];
 
   schallernetz = { };
 
-  system.stateVersion = "23.11";
+  system.stateVersion = "23.11"; # move this line to hardware-configuration.nix
 }
