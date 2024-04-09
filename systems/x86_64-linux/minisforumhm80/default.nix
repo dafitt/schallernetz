5d7b7eb2e4ee6e481d1c4161d66b8e4c4c38d67d
dafitt <dafitt@posteo.me>
2024-04-09 21:06:06 +0200

    refactor!: snowfall-lib test & build

--- systems/x86_64-linux/minisforumhm80/default.nix
+++ systems/x86_64-linux/minisforumhm80/default.nix
@@ -1,8 +1,8 @@
 { ... }: {
   #$ nix repl .#nixosConfigurations.minisforumhm80
   #$ nix build .#nixosConfigurations.minisforumhm80.config.system.build.toplevel
-  #$ nixos-rebuild build --flake .#minisforumhm80 --show-trace
-  #$ sudo nixos-rebuild <test|switch|boot> --flake .#minisforumhm80
+  #$ nixos-rebuild build --fast --flake .#minisforumhm80 --show-trace
+  #$ ssh-add ~/.ssh/minisforumhm80 && nixos-rebuild --flake .#minisforumhm80 --target-host admin@minisforumhm80 --use-remote-sudo <test|boot|switch>
 
   imports = [
     ./hardware-configuration.nix
