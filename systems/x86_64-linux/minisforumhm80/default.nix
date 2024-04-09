a5910c53d00a8dc5149064cd9357b79f11737d78
dafitt <dafitt@posteo.me>
2024-04-09 21:12:58 +0200

    systems: added custom library

--- systems/x86_64-linux/minisforumhm80/default.nix
+++ systems/x86_64-linux/minisforumhm80/default.nix
@@ -1,9 +1,9 @@
-{ ... }: {
-  #$ nix repl .#nixosConfigurations.minisforumhm80
-  #$ nix build .#nixosConfigurations.minisforumhm80.config.system.build.toplevel
-  #$ nixos-rebuild build --fast --flake .#minisforumhm80 --show-trace
-  #$ ssh-add ~/.ssh/minisforumhm80 && nixos-rebuild --flake .#minisforumhm80 --target-host admin@minisforumhm80 --use-remote-sudo <test|boot|switch>
+#$ nix repl .#nixosConfigurations.minisforumhm80
+#$ nix build .#nixosConfigurations.minisforumhm80.config.system.build.toplevel
+#$ nixos-rebuild build --fast --flake .#minisforumhm80 --show-trace
+#$ ssh-add ~/.ssh/minisforumhm80 && nixos-rebuild --flake .#minisforumhm80 --target-host admin@minisforumhm80 --use-remote-sudo <test|boot|switch>
 
+{ lib, ... }: with lib.schallernetz; {
   imports = [
     ./hardware-configuration.nix
     ./configuration.nix
--- templates/system/default.nix
+++ templates/system/default.nix
@@ -1,9 +1,9 @@
-{ ... }: {
-  #$ nix repl .#nixosConfigurations.<host>
-  #$ nix build .#nixosConfigurations.<host>.config.system.build.toplevel
-  #$ nixos-rebuild build --fast --flake .#<host> --show-trace
-  #$ ssh-add ~/.ssh/<host> && nixos-rebuild --flake .#<host> --target-host admin@<host> --use-remote-sudo <test|switch|boot>
+#$ nix repl .#nixosConfigurations.<host>
+#$ nix build .#nixosConfigurations.<host>.config.system.build.toplevel
+#$ nixos-rebuild build --fast --flake .#<host> --show-trace
+#$ ssh-add ~/.ssh/<host> && nixos-rebuild --flake .#<host> --target-host admin@<host> --use-remote-sudo <test|switch|boot>
 
+{ lib, ... }: with lib.schallernetz; {
   imports = [ ./hardware-configuration.nix ];
 
   schallernetz = { };
