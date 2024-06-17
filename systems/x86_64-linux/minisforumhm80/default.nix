e108f44a2f25c8c7253f5f0c200b5016a4852c94
dafitt <dafitt@posteo.me>
2024-06-17 14:55:39 +0200

    templates: updated commands

--- systems/x86_64-linux/minisforumhm80/default.nix
+++ systems/x86_64-linux/minisforumhm80/default.nix
@@ -1,7 +1,4 @@
-#$ nix repl
-#nix-repl> :lf .
 #nix-repl> nixosConfigurations.minisforumhm80.config
-
 #$ nix build .#nixosConfigurations.minisforumhm80.config.system.build.toplevel
 #$ nixos-rebuild build --fast --flake .#minisforumhm80 --show-trace
 #$ ssh-add ~/.ssh/minisforumhm80 && nixos-rebuild --flake .#minisforumhm80 --target-host admin@minisforumhm80.***REMOVED_DOMAIN*** --use-remote-sudo <test|boot|switch>
