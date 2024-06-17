a0f7893775ae4ff49dea1d945b4bf09c01d6e0de
dafitt <dafitt@posteo.me>
2024-06-17 14:55:39 +0200

    templates: updated commands

--- templates/system/default.nix
+++ templates/system/default.nix
@@ -1,5 +1,19 @@
-#$ nix repl .#nixosConfigurations.<host>
+# Check:
+#$ nix flake check
+#$ nix repl
+#nix-repl> :lf .
+#nix-repl> nixosConfigurations.<host>.config
+
+# Build:
+#$ flake build-system [#<host>]
+#$ nixos-rebuild build --fast --flake .#<host> --show-trace
 #$ nix build .#nixosConfigurations.<host>.config.system.build.toplevel
+
+# Activate:
+#$ flake <test|switch|boot> [#<host>]
+#$ nixos-rebuild --flake .#<host> <test|switch|boot>
+#$ nix run .#nixosConfigurations.<host>.config.system.build.toplevel
+
 { options, config, lib, pkgs, inputs, ... }:
 
 with lib;
