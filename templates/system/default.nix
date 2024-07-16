3f81248ee94d908dda753fe1f436d186c16c5747
dafitt <dafitt@posteo.me>
2024-07-16 11:36:39 +0200

    templates: added schallernetz options with defaults

--- templates/system/default.nix
+++ templates/system/default.nix
@@ -20,7 +20,58 @@ with lib;
 with lib.schallernetz; {
   imports = [ ./hardware-configuration.nix ];
 
-  schallernetz = rec { };
+  schallernetz = rec {
+    backups.localhost = true;
+    backups.NAS4 = true;
+    backups.paths = [ ];
+
+    containers.adguard.enable = false;
+    containers.adguard.name = "adguard";
+    containers.adguard.ipv6address = "***REMOVED_IPv6***";
+    containers.bitwarden.enable = false;
+    containers.bitwarden.name = "bitwarden";
+    containers.bitwarden.ipv6address = "***REMOVED_IPv6***";
+    containers.DavidCAL.enable = false;
+    containers.DavidCAL.name = "DavidCAL";
+    containers.DavidCAL.ipv6address = "***REMOVED_IPv6***";
+    containers.DavidCAL.remoteBackups = true;
+    containers.DavidSYNC.enable = false;
+    containers.DavidSYNC.name = "DavidSYNC";
+    containers.DavidSYNC.ipv6address = "***REMOVED_IPv6***";
+    containers.DavidVPN.enable = false;
+    containers.DavidVPN.name = "DavidVPN";
+    containers.MichiSHARE.enable = false;
+    containers.MichiSHARE.name = "MichiSHARE";
+    containers.MichiSHARE.ipv6address = "***REMOVED_IPv6***";
+    containers.ntfy.enable = false;
+    containers.ntfy.name = "ntfy";
+    containers.ntfy.ipv6address = "***REMOVED_IPv6***";
+    containers.searx.enable = false;
+    containers.searx.name = "searx";
+    containers.searx.ipv6address = "***REMOVED_IPv6***";
+    containers.unbound.enable = false;
+    containers.unbound.name = "unbound";
+    containers.unbound.ipv6address = "***REMOVED_IPv6***";
+
+    environment.enable = true;
+
+    locale.enable = true;
+
+    networking.enable = true;
+
+    nix.enable = true;
+
+    services.haproxy.enable = true;
+    services.haproxy.frontends.www.extraConfig = [ ];
+    services.ssh.enable = true;
+
+    time.enable = true;
+
+    users.admin.enable = true;
+    users.rebuild.enable = true;
+    users.root.enable = true;
+    users.root.allowSshPasswordAuthentication = false;
+  };
 
   environment.systemPackages = with pkgs; [
   ];
