67446b4552c0afc95190e82f61b0ff1ef1f9ce9c
david <david.schaller@posteo.net>
2024-09-25 11:15:25 +0200

    wireguard: added a client.conf generation script

---
+++ modules/nixos/servers/wireguard/client.conf.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env nix
+#!nix shell
+#!nix nixpkgs#wireguard-tools
+#!nix nixpkgs#hexdump
+#!nix nixpkgs#qrencode
+#!nix nixpkgs#bash --command bash
+
+# Run this script to generate a new wireguard client: #$ env nix "./client.conf.sh"
+# It creates the `client.conf` file, outputs the QR-Code and the corresponding NixOS configuration
+
+INFO="[\e[1;34m INFO \e[0m]"     # [ INFO ]
+ACTION="[\e[1;33m ACTION \e[0m]" # [ ACTION ]
+HELP="[\e[1;36m HELP \e[0m]"     # [ HELP ]
+
+echo -ne "$ACTION Hostname/FQDN/Identifier of the device [client]> " && read name && [ -z "$name" ] && name="client"
+filepath="$(dirname $0)/$name.conf"
+ip6Address="fc01::$(hexdump -n 2 -e '"%03x"' </dev/urandom | cut -c1-3)/64"
+privateKey="$(wg genkey)"
+publicKey="$(echo $privateKey | wg pubkey)"
+presharedKey="$(wg genpsk)"
+
+echo -e "$INFO File:\n$filepath"
+cat <<EOL >$filepath
+[Interface]
+Address = $ip6Address
+ListenPort = 51902
+PrivateKey = $privateKey
+
+[Peer]
+PublicKey = D90wpk+4Hwpvnmq8JUqcjoph1tswJ5sZRKwvmKSlLnw
+PresharedKey = $presharedKey
+AllowedIPs = ***REMOVED_IPv6***::/56, ***REMOVED_IPv4***/23
+Endpoint = wireguard.***REMOVED_DOMAIN***:123
+EOL
+
+echo -e "$INFO QR-Code:"
+qrencode -t ansiutf8 -r $filepath
+
+echo -e "$ACTION NixOS configuration:"
+cat <<EOL
+config.wireguard.interfaces.<name>.peers = [
+  {
+    # $name
+    publicKey = "$publicKey";
+    presharedKey = "$presharedKey";
+    allowedIPs = [ "$ip6Address" ];
+  }
+];
+EOL
+
+echo -e $HELP
+cat <<EOL
+To import into Networkmanager run:
+sudo nmcli connection import type wireguard file $name.conf
+EOL
