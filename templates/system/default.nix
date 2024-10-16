f8939ecf5c4c7ef3d717da2ecf62d3994b46abda
david <david.schaller@posteo.net>
2024-10-16 21:56:53 +0200

    templates: completed server options

--- templates/system/default.nix
+++ templates/system/default.nix
@@ -24,9 +24,12 @@ with lib.schallernetz; {
   ];
 
   schallernetz = rec {
-    backups.localhost = true;
-    backups.NAS4 = true;
+    # NOTE these are the defaults
+    backups.localhost = false;
+    backups.NAS4 = false;
+    backups.magentacloudMICHI = false;
     backups.paths = [ ];
+    backups.pauseServices = [ ];
 
     servers.adguardhome.enable = false;
     servers.adguardhome.name = "adguardhome";
@@ -34,28 +37,52 @@ with lib.schallernetz; {
     servers.adguardhome.ip6HostAddress = ":8";
     servers.bitwarden.enable = false;
     servers.bitwarden.name = "bitwarden";
+    servers.bitwarden.subnet = "server";
+    servers.bitwarden.ip6HostAddress = ":b51";
     servers.DavidCAL.enable = false;
     servers.DavidCAL.name = "DavidCAL";
+    servers.DavidCAL.subnet = "server";
+    servers.DavidCAL.ip6HostAddress = ":297";
     servers.DavidSYNC.enable = false;
     servers.DavidSYNC.name = "DavidSYNC";
-    servers.DavidVPN.enable = false;
-    servers.DavidVPN.name = "DavidVPN";
+    servers.DavidSYNC.subnet = "server";
+    servers.DavidSYNC.ip6HostAddress = ":2b6";
     servers.forgejo.enable = false;
     servers.forgejo.name = "forgejo";
+    servers.forgejo.subnet = "server";
+    servers.forgejo.ip6HostAddress = ":7b9";
     servers.haproxy-server.enable = false;
     servers.haproxy-server.name = "haproxy-server";
+    servers.haproxy-server.subnet = "server";
+    servers.haproxy-server.ip6HostAddress = ":7fc";
+    servers.haproxy-dmz.enable = false;
+    servers.haproxy-dmz.name = "haproxy-dmz";
+    servers.haproxy-dmz.subnet = "server";
+    servers.haproxy-dmz.ip6HostAddress = ":7fd";
     servers.MichiSHARE.enable = false;
     servers.MichiSHARE.name = "MichiSHARE";
+    servers.MichiSHARE.subnet = "server";
+    servers.MichiSHARE.ip6HostAddress = ":c66";
     servers.ntfy.enable = false;
     servers.ntfy.name = "ntfy";
+    servers.ntfy.subnet = "server";
+    servers.ntfy.ip6HostAddress = ":e73";
     servers.searx.enable = false;
     servers.searx.name = "searx";
+    servers.searx.subnet = "dmz";
+    servers.searx.ip6HostAddress = ":89c";
     servers.torRelay.enable = false;
     servers.torRelay.name = "torRelay";
     servers.torRelay.subnet = "dmz";
     servers.torRelay.ip6HostAddress = ":58b";
     servers.unbound.enable = false;
     servers.unbound.name = "unbound";
+    servers.unbound.subnet = "server";
+    servers.unbound.ip6HostAddress = ":9";
+    servers.wireguard-lan.enable = false;
+    servers.wireguard-lan.name = "wireguard-lan";
+    servers.wireguard-lan.subnet = "lan";
+    servers.wireguard-lan.ip6HostAddress = ":ef5";
 
     environment.enable = true;
 
@@ -66,7 +93,7 @@ with lib.schallernetz; {
     nix.enable = true;
 
     ntfy-systemd.enable = true;
-    ntfy-systemd.url = "https://ntfy.***REMOVED_DOMAIN***";
+    ntfy-systemd.url = "https://ntfy.lan.***REMOVED_DOMAIN***";
 
     ssh.enable = true;
 
@@ -108,7 +135,7 @@ with lib.schallernetz; {
     };
   };
 
-  # add device-specific nixos configuration here #
+  # <<< add device-specific nixos configuration here
 
   system.stateVersion = "24.05";
 }
